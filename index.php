081f005bde7a45443f2d1fc41cf66c861f9a9089
Majdalshanteer <108790509+Majdalshanteer@users.noreply.github.com>
2022-09-14 23:41:27 +0300

    edit

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ $phones = [
     [ 
      'name' => 'INFINIX Zero 8',
      'img_url' =>'https://eshop.orange.jo/app-images/thumbs/0001278_infinix-zero-8_220.jpeg',
-    'rate' => '0',
+    'rate' => '2',
     'brand' => 'Infinix',
     'price' => 'JOD 205.00',
     'is_out_of_stock' => '1'
@@ -19,7 +19,7 @@ $phones = [
     [ 
      'name' => 'Apple iPhone 12 Pro',
      'img_url' =>'https://eshop.orange.jo/app-images/thumbs/0001495_apple-iphone-12-pro_220.jpeg',
-    'rate' => '0',
+    'rate' => '5',
     'brand' => 'Apple',
     'price' => 'JOD 973.00',
     'is_out_of_stock' => '1'
@@ -102,6 +102,58 @@ $phones = [
     </div>
   </div>
 </nav>
+<div class="row row-cols-1 mt-3 row-cols-md-3 g-4 mx-5">
+
+
+<?php
+
+for($i=0;$i<count($phones);$i++){
+ 
+?>
+<div class="col">
+<div class="card h-70" style="width:350px" >
+  <img style="height:150px;width:140px;" src="<?php echo $phones[$i]['img_url']?>" class="card-img-top rounded mx-auto d-block mt-4 " alt="...">
+  <div class="card-body">
+  <p><?php echo $phones[$i]['brand']?></p>
+    <h5 class="card-title"><?php echo $phones[$i]['name']?></h5>
+    <p class="card-text">
+    
+    <?php
+for($k=0;$k<5;$k++){
+  if ($k < $phones[$i]['rate']){
+    echo  '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-star-fill" viewBox="0 0 16 16">
+    <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
+  </svg>';
+  }
+else{
+  echo '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-star" viewBox="0 0 16 16">
+  <path d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.522-3.356c.33-.314.16-.888-.282-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767-3.686 1.894.694-3.957a.565.565 0 0 0-.163-.505L1.71 6.745l4.052-.576a.525.525 0 0 0 .393-.288L8 2.223l1.847 3.658a.525.525 0 0 0 .393.288l4.052.575-2.906 2.77a.565.565 0 0 0-.163.506l.694 3.957-3.686-1.894a.503.503 0 0 0-.461 0z"/>
+</svg>';
+}
+}
+?>
+    
+    <?php echo '('.$phones[$i]['rate'].')'?></p>
+    <a href="#"  >View Details</a>
+    <hr> 
+    <h4> <?php $phones[$i]['price']?></h4>
+    <a href="#"  class="btn btn-primary px-5 ">Buy Now</a>
+    <svg xmlns="http://www.w3.org/2000/svg" width="50" height="40" fill="currentColor" class="bi bi-cart-fill mx-4" viewBox="0 0 16 16">
+  <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
+</svg>
+
+   
+  </div>
+</div>
+</div>
+
+
+
+<?php
+}
+
+
+?>
 <script src="https://cdn.jsdelivr.net/npm/boosted@5.1.3/dist/js/boosted.bundle.min.js" integrity="sha384-5thbp4uNEqKgkl5m+rMBhqR+ZCs+3iAaLIghPWAgOv0VKvzGlYKR408MMbmCjmZF" crossorigin="anonymous"></script>
 </body>
 </html>
