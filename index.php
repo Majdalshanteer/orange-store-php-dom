d0a115a5a3af04bb6bb05634f630ec83fc206cdb
Majdalshanteer <108790509+Majdalshanteer@users.noreply.github.com>
2022-09-15 08:52:56 +0300

    edit

--- index.php
+++ index.php
@@ -104,7 +104,6 @@ $phones = [
 </nav>
 <div class="row row-cols-1 mt-3 row-cols-md-3 g-4 mx-5">
 
-
 <?php
 
 for($i=0;$i<count($phones);$i++){
@@ -122,7 +121,7 @@ for($i=0;$i<count($phones);$i++){
 for($k=0;$k<5;$k++){
   if ($k < $phones[$i]['rate']){
     //fill star
-    echo  '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-star-fill" viewBox="0 0 16 16">
+    echo  '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#ffcf27" class="bi bi-star-fill" viewBox="0 0 16 16">
     <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
   </svg>';
   }
@@ -136,9 +135,11 @@ else{
 ?>
     
     <?php echo '('.$phones[$i]['rate'].')'?></p>
-    <a href="#"  >View Details</a>
+    <a href="#"  >View Details <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right-short" viewBox="0 0 16 16">
+  <path fill-rule="evenodd" d="M4 8a.5.5 0 0 1 .5-.5h5.793L8.146 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L10.293 8.5H4.5A.5.5 0 0 1 4 8z"/>
+</svg></a>
     <hr> 
-    <h4> <?php echo $phones[$i]['price']?></h4>
+    <span style="color:orange;"><h4> <?php echo $phones[$i]['price']?></h4></span>
     <a href="#"  class="btn btn-primary px-5 ">Buy Now</a>
     <svg xmlns="http://www.w3.org/2000/svg" width="50" height="40" fill="currentColor" class="bi bi-cart-fill mx-4" viewBox="0 0 16 16">
   <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
